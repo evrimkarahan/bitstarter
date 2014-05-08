e9871bb5e765202d724e1c9fd4d8f5775278b35b
Evrim Karahan <evrimtarimer@gmail.com>
2014-05-07 22:48:12 -0700

    added fs.readFile for index.html

--- web.js
+++ web.js
@@ -1,8 +1,13 @@
+var buffer = new buffer(fs.readFile('index.html', function(err,data){
+	    if(err) throw err;
+	    console.log(data);
+	}, "utf-8"));
+
 var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request , response){
-	response.send('Hello World2');
+	response.send(buffer.toString('utf-8'));
     });
 
 var port = process.env.PORT || 5000;
